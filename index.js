8942da285f3a95cd27865acd55ba98ddccf56596
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-28 21:54:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function theBeatlesPlay(musicians, instruments){
 
 
 function johnLennonFacts(facts){
-  while (var i< facts.length){
+  vari= 0
+  while ( i< facts.length){
     facts[i]=facts[i] + "!!!"
   }
 }
